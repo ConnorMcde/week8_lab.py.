3cb26d304c92c4ae48eb14dfd88769940af93a64
ConnorMcde <26596059@edgehill.ac.uk>
2025-11-25 19:28:01 +0000

    Add files via upload

---
+++ Week 8 exercise 4.py
@@ -0,0 +1,17 @@
+"""Week 8 exercise 4 """
+
+import requests # Import the tool to talk to the web
+url = "https://official-joke-api.appspot.com/random_joke"
+# 1. Send the request
+print("Calling the server...")
+response = requests.get(url)
+# 2. Check the Status Code (200 = Success, 404 = Not Found)
+print(f"Status Code: {response.status_code}")
+# 3. See the raw text data
+print("Raw Data:")
+print(response.text)
+
+#run three times does the text change and why
+
+#the text does change and its the joke and the id
+#why it changes is because it pulls a random entry from the api each time
